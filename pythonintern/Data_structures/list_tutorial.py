bc966ffd908495c151858e4fe7c3a2101bd1d308
KATWA <KATWA@LAPTOP-0IHQR5SD>
2025-04-06 13:42:56 +0530

    06/04/2025

---
+++ pythonintern/Data_structures/list_tutorial.py
@@ -0,0 +1,34 @@
+'''
+Created on 6 Apr 2025
+
+@author: Bhavani
+
+List :
+
+1.creation of list
+'''
+a=[]
+print(type(a))
+
+a={1}
+print(type(a))
+
+a=()
+print(type(a))
+
+# creation of list
+ 
+b=[1, 2, 3, 4, 5]  #homogeneous data structure
+print(type(b))
+print(b)
+
+c=[1, "abc", 4.27, True, 3+8j]    #heterogeneous structure
+print(c)
+
+d=list(range(10))
+print(d)
+
+
+
+
+
