b7fe5fd98cbce70795a2e204885ceba6a5744d78
KATWA <KATWA@192.168.1.38>
2025-04-12 12:28:06 +0530

    work

--- pythonintern/Data_structures/list_tutorial.py
+++ pythonintern/Data_structures/list_tutorial.py
@@ -2,11 +2,12 @@
 Created on 6 Apr 2025
 
 @author: Bhavani
-
-List :
-
-1.creation of list
 '''
+
+#List :
+
+#1.creation of list
+
 a=[]
 print(type(a))
 
@@ -25,10 +26,88 @@ print(b)
 c=[1, "abc", 4.27, True, 3+8j]    #heterogeneous structure
 print(c)
 
-d=list(range(10))
+d=list(range(1,20))
 print(d)
 
+#ACCESSING THE ELEMENTS USING INDEX
+print(c[4])
+print(c[-1])
 
+#not modified for set
 
+z=(1,3,7,9,11)
+z[2]=22
+print(z)    #not modified for tuple
 
+print(c[::])
+print(d[3:9])
 
+#slicing operator-list_name[start : stop : step] --->index
+#
+
+print('d[::]-->', d[::])
+print('d[3:9]-->', d[3:9])
+print('d[:12]-->', d[:12])
+print('d[6:]-->', d[6:])
+print('d[-12:-6]-->', d[-12:-6])
+print('d[::2]-->', d[::2])
+print('d[::-1]-->', d[::-1])
+print('d[-12:-6:-1]-->', d[-12:-6:-1])
+
+#accessing for loops
+
+#for loop:
+print("====for loop===")
+for ele in c:
+    print(ele)
+    
+#====while loop====
+i=0
+while i<5:
+    print(c[i])
+    i+=1
+    
+    print("====while loop====")
+    print(c[0])
+    print(c[1])
+    print(c[2])
+    print(c[3])
+
+    
+for n in d:
+    if n%2==1:
+        print(n)
+        
+        
+e=["Bhavani", "chithra","sandeep","yogitha","vivek","sanjana"]
+for g in e:
+    if 'a' in g:
+        print(g)
+        
+        
+h=["bhavani", "chitra", "sandeep", "sanjana", "vivek" ,"yogitha", "manju"]
+h.append("vinu")
+print(h)
+
+h.insert(4,"padma")
+print(h)
+
+f=[15,30,22,10,60,13]
+for n in f:
+    if n%2==1:
+        print(n**2)
+        
+j=[]
+k=[15,30,22,10,60,13]
+for n in k:
+    if n%2==1:
+        j.append(n**2)
+print(j)
+        
+
+    
+    
+    
+    
+    
+    
---
+++ pythonintern/basics/looping.py
@@ -0,0 +1,146 @@
+'''
+Created on 2 Apr 2025
+
+@author: KATWA
+
+#from itertools import count
+'''
+
+count=5
+while count>0:
+    print("hi world")
+    count=count-1
+    
+    
+# looping statement  
+#while loop- repeated actions
+count=0
+while count<=100:
+    if count == 50:
+        count+=1
+        continue 
+    print(count)
+    count+=1
+    
+
+#for loop - To iterate collections or range
+
+print (range(10))   
+
+    
+    
+print(range(10))
+
+
+for n in range(1,50):
+    print(n)    
+    
+    
+for n in range(1,100):
+    if n==50:
+        continue
+    print(n)
+  
+# for even number
+
+for n in range(1,100):
+    if n%2==0:
+        print(n)
+    
+#for odd number
+for n in range(1,100):
+    if n%2==1:
+        if n==11:       #To check the condition 
+            continue
+        print(n)
+    
+for n in range(1,100,2):
+    print(n) 
+    
+    
+    
+    
+    '''
+    
+*                *****
+**               ****
+***              ***
+****             **
+'''
+
+    
+for n in range(1, 6):
+   
+    for m in range (n): 
+        print("*", end=" ")
+        
+    print()
+    
+    
+for n in range(4,0,-1):
+    
+    for m in range (n):
+        print("*", end=" ")
+        
+    print()
+    
+    #
+        
+for n in range(1, 6):
+    
+    
+    for m in range(6-n):
+        print(" ", end="")
+    
+    for i in range(n):
+        print("*", end=" ")
+        
+    print()
+    
+    #
+for n in range(1, 6):    
+    
+    
+    for m in range(6-n):
+        print(" ", end="")
+        
+    for i in range(n):
+        print("1", end=" ")   
+        
+    print()
+    
+
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        
