b71b100051065e895d08c956bf67a6b91720765f
KATWA <KATWA@LAPTOP-0IHQR5SD>
2025-05-04 14:22:28 +0530

    04/05

--- pythonintern/function/my_functions.py
+++ pythonintern/function/my_functions.py
@@ -3,6 +3,7 @@ Created on 27 Apr 2025
 
 @author: bhavani
 '''
+''
 a=6
 b=7
 c=a+b
@@ -11,3 +12,9 @@ def welcome():
     print("hi everyone")
 welcome()
 
+def addition(a,b):
+    c=a+b
+    print("sum of {a} and {b}:",c)
+    return c
+#d=addition(10,20)
+#print(d)
---
+++ pythonintern/oops_concept/oops_basic.py
@@ -0,0 +1,18 @@
+'''
+Created on 4 May 2025
+
+@author: bhavani
+'''
+class Human_Being():
+    def walk(self):
+        print("i am walking")
+
+
+Obj1=Human_Being()
+Obj1.walk()
+
+print(type(Obj1))
+
+Obj2=Human_Being()
+Obj2.walk()
+    
